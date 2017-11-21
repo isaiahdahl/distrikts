710413f722e26cf42401b851127c6489d5bec7de
isaiah dahl <isaiahdahl@me.com>
2017-11-21 00:36:25 +0000

    missed deleting some autocomplete stuff i was playing around with

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :distrikts, only: [:index, :show, :new, :create] do
-    get :autocomplete_distrikt_name, :on => :collection
-  end
+  resources :distrikts, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
