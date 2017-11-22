c91cb05389d43d8793fd9db0cd9589ff1b5830b8
isaiah dahl <isaiahdahl@me.com>
2017-11-22 11:13:05 +0000

    admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121213554) do
+ActiveRecord::Schema.define(version: 20171122110525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 20171121213554) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["score_id"], name: "index_users_on_score_id"
