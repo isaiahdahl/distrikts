178ab6b9fe37776c25af29574c193d8f58488c5f
Guillaume Barthere <gbarthere@yahoo.com>
2017-11-29 14:59:37 +0000

    Metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
